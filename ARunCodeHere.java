44bef6e684fbd59337fda6df928ea0d232926fbc
BigFish <junhongeu030324@gmail.com>
2024-03-04 11:51:58 +0800

    Edited AdminEdit

--- ARunCodeHere.java
+++ ARunCodeHere.java
@@ -1,31 +1,31 @@
 public class ARunCodeHere {
     
-    // static MainPage a;
-    // static PageAdmin b;
-    // static PageAdmin_Register c;
+    static MainPage a;
+    static PageAdmin b;
+    static PageAdmin_Register c;
     static PageAdmin_Edit d;
-    // static RegisterSuccess e;
-    // static EditConfirmation f;
-    // static AboutUs g;
-    // static Slider h;
+    static RegisterSuccess e;
+    static EditConfirmation f;
+    static AboutUs g;
+    static Slider h;
 
     public static void main(String[] args) {
-        // a = new MainPage();
-        // b = new PageAdmin();
-        // c = new PageAdmin_Register();
+        a = new MainPage();
+        b = new PageAdmin();
+        c = new PageAdmin_Register();
         d = new PageAdmin_Edit();
-        // e = new RegisterSuccess();
-        // f = new EditConfirmation();
-        // g = new AboutUs();
-        // h = new Slider();
+        e = new RegisterSuccess();
+        f = new EditConfirmation();
+        g = new AboutUs();
+        h = new Slider();
 
-        // MainPage.container.setVisible(true);
-        // PageAdmin.container.setVisible(false);
-        // PageAdmin_Register.container.setVisible(false);
-        PageAdmin_Edit.container.setVisible(true);
-        // RegisterSuccess.container.setVisible(false);
-        // EditConfirmation.container.setVisible(false);
-        // AboutUs.container.setVisible(false);
-        // Slider.frame.setVisible(false);
+        MainPage.container.setVisible(true);
+        PageAdmin.container.setVisible(false);
+        PageAdmin_Register.container.setVisible(false);
+        PageAdmin_Edit.container.setVisible(false);
+        RegisterSuccess.container.setVisible(false);
+        EditConfirmation.container.setVisible(false);
+        AboutUs.container.setVisible(false);
+        Slider.frame.setVisible(false);
     }    
 }
