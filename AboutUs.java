ae66f5416f09a0ba16c109a3861e36ecfca765d7
BigFish <junhongeu030324@gmail.com>
2024-03-04 16:52:57 +0800

    Edited AboutUs

--- AboutUs.java
+++ AboutUs.java
@@ -18,26 +18,22 @@ public class AboutUs implements ActionListener{
 
     public AboutUs(){
         container = new JFrame("APU Hostel Home Appliances Service Centre (AHHASC)");
-        container.setSize(360,150);//GUI Size
+        container.setSize(800,600);//GUI Size
         container.setLocationRelativeTo(null);//GUI Location
         container.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         container.setResizable(false);//Fix the GUI Size
         container.setLayout(null);
 
-        aboutUsText = new JLabel("<html>Are you sure that you want to continue the <br>action?</html>");//asking sentence
-        aboutUsText.setBounds(15, 0, 330, 50);
-        aboutUsText.setFont(new Font("Times New Roman", Font.PLAIN, 17));
+        aboutUsText = new JLabel("APU Hostel Home Appliances Service Centre (AHHASC) is strategically located at Technology Park Malaysia, Bukit Jalil, Kuala Lumpur. TPM is the ideal location for this new and contemporary hostel due to its strong positioning as Malaysia's primary hub for leading-edge and high-tech developments in a wide variety of areas such as Information and Communication Technology, Biotechnology, Microelectronics, Engineering and Broadcasting. TPM is also located in one of the most rapidly developing areas in Kuala Lumpur, and is well served and accessible through major highways, LRT and other forms of public transportation.");//aboutus sentence
+        aboutUsText.setBounds(15, 0, 770, 50);
+        aboutUsText.setFont(new Font("Times New Roman", Font.PLAIN, 14));
 
         ok = new JButton("OK");//confirm Button
-        ok.setBounds((container.getWidth()/2)+45, 70, 100, 30);
+        ok.setBounds((container.getWidth()/2)-260, container.getHeight()-100, 500, 30);
         ok.addActionListener(this);
 
         container.add(ok);
         container.add(aboutUsText);
         container.setVisible(true);
-    }
-
-    public class Slider {
-        a
     }
 }
