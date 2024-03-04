a44fda61f0b53d17ff37d9914e55daf3996bf171
BigFish <junhongeu030324@gmail.com>
2024-03-04 12:05:43 +0800

    change runcode page

--- ARunCodeHere.java
+++ ARunCodeHere.java
@@ -1,29 +1,21 @@
 public class ARunCodeHere {
     
-    // static MainPage a;
-    // static PageAdmin b;
-    // static PageAdmin_Register c;
+    static MainPage a;
+    static PageAdmin b;
+    static PageAdmin_Register c;
     static PageAdmin_Edit d;
-<<<<<<< HEAD
     static RegisterSuccess e;
     static EditConfirmation f;
-    // static AboutUs g;
-=======
-    // static RegisterSuccess e;
-    // static EditConfirmation f;
-    // static AboutUs g;
-    // static Slider h;
+    static AboutUs g;
 
->>>>>>> parent of 44bef6e (Edited AdminEdit)
     public static void main(String[] args) {
-        // a = new MainPage();
-        // b = new PageAdmin();
-        // c = new PageAdmin_Register();
+        a = new MainPage();
+        b = new PageAdmin();
+        c = new PageAdmin_Register();
         d = new PageAdmin_Edit();
-<<<<<<< HEAD
         e = new RegisterSuccess();
         f = new EditConfirmation();
-        // g = new AboutUs();
+        g = new AboutUs();
 
         MainPage.container.setVisible(true);
         PageAdmin.container.setVisible(false);
@@ -31,21 +23,6 @@ public class ARunCodeHere {
         PageAdmin_Edit.container.setVisible(false);
         RegisterSuccess.container.setVisible(false);
         EditConfirmation.container.setVisible(false);
-        // AboutUs.container.setVisible(false);
-=======
-        // e = new RegisterSuccess();
-        // f = new EditConfirmation();
-        // g = new AboutUs();
-        // h = new Slider();
-
-        // MainPage.container.setVisible(true);
-        // PageAdmin.container.setVisible(false);
-        // PageAdmin_Register.container.setVisible(false);
-        PageAdmin_Edit.container.setVisible(true);
-        // RegisterSuccess.container.setVisible(false);
-        // EditConfirmation.container.setVisible(false);
-        // AboutUs.container.setVisible(false);
-        // Slider.frame.setVisible(false);
->>>>>>> parent of 44bef6e (Edited AdminEdit)
+        AboutUs.container.setVisible(false);
     }    
 }
