208f4d76ad0ab9fbf2dde22af51d620099d28760
BigFish <junhongeu030324@gmail.com>
2024-03-19 20:34:53 +0800

    Update PageTechnician_Feedback.java

--- PageTechnician_Feedback.java
+++ PageTechnician_Feedback.java
@@ -8,6 +8,7 @@ import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 
 import javax.swing.JButton;
+import javax.swing.JComboBox;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
@@ -38,7 +39,6 @@ public class PageTechnician_Feedback implements ActionListener{
 
     static JFrame container;
     static JTextField userDisplay, dateDisplay;
-    JTable table;
     JTextArea descriptionDisplay;
     JLabel customerText, feedbackText, userText, dateText, descriptionText;
 
@@ -46,7 +46,7 @@ public class PageTechnician_Feedback implements ActionListener{
 
         // Frame of this page
         container = new JFrame("APU Hostel Home Appliances Service Centre (AHHASC)");
-        container.setSize(1000, 600);
+        container.setSize(750, 600);
         container.setLocationRelativeTo(null);
         container.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         container.setLayout(null);
@@ -61,87 +61,46 @@ public class PageTechnician_Feedback implements ActionListener{
             }
         });
 
-        // Left Top Panel
-        // Create Panel
-        JPanel leftTopPanel = new JPanel();
-        leftTopPanel.setLayout(new FlowLayout());
-        leftTopPanel.setBounds(45, 5, 260, 40);
 
-        // Customer Text
-        customerText = new JLabel("Customer");
-        customerText.setBounds((leftTopPanel.getWidth() - 50)/2, 0, 140, 50);
-        customerText.setFont(new Font("Times New Roman", Font.BOLD, 24));
 
-        // Left Below Panel
-        // Create Panel
-        JPanel leftBelowPanel = new JPanel();
-        leftBelowPanel.setLayout(new FlowLayout());
-        leftBelowPanel.setPreferredSize(new Dimension(100, 600));
-
-        // Table Section
-        // Table Dataset (Text File)
-        Object[] roleNames = {"TestColumn"};  // Use final for constant values
-
-        String[][] data = { {"TestRow1"}, {"TestRow2"}, {"TestRow3"} };
-
-        // Table model and create table
-        DefaultTableModel model = new DefaultTableModel(data, roleNames);
-        table = new JTable(model);
-
-        // Set column headers directly
-        model.setColumnIdentifiers(roleNames);
-
-        // Disable table editing
-        table.setDefaultEditor(Object.class, null);
-
-        // Table Frame
-        table.setFont(new Font("Times New Roman", Font.BOLD, 16));
-        TableColumnModel columnModel = table.getColumnModel();
-        columnModel.getColumn(0).setPreferredWidth(280);
-        table.setRowHeight(50);
-        table.setAutoCreateRowSorter(true);
-
-        // Scroll Pane for Left Below Panel
-        JScrollPane scrollPane = new JScrollPane(leftBelowPanel);
-        scrollPane.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_ALWAYS);
-        scrollPane.setBounds(10, 50, 300, 500);
-        scrollPane.setViewportView(leftBelowPanel);
-
-        // Right Panel
-        // Create Panel
-        JPanel rightPanel = new JPanel();
-        rightPanel.setLayout(null);
-        rightPanel.setBounds(310, 10, 660, 540);
+        // Description Panel
+        JPanel descriptionPanel = new JPanel();
+        descriptionPanel.setLayout(null);
+        descriptionPanel.setBounds(65, 280, 600, 250);
+        JComboBox<String> usernameBox;
 
         // Text Section
         // Feedback Text
         feedbackText = new JLabel("Feedback");
-        feedbackText.setBounds((rightPanel.getWidth()/2) + 260, 50, 140, 50);
+        feedbackText.setBounds(descriptionPanel.getWidth()/2, 50, 140, 50);
         feedbackText.setFont(new Font("Times New Roman", Font.BOLD, 24));
 
         // Username Text
         userText = new JLabel("Username");
-        userText.setBounds(((rightPanel.getWidth() + 10)/2)+5, 100, 350, 30);
+        userText.setBounds(70, 100, 350, 30);
         userText.setFont(new Font("Times New Roman", Font.BOLD, 16));
 
         // Date text
         dateText = new JLabel("Date");
-        dateText.setBounds(((rightPanel.getWidth() + 10)/2)+5, 170, 350, 30);
+        dateText.setBounds(70, 170, 350, 30);
         dateText.setFont(new Font("Times New Roman", Font.BOLD, 16));
 
         // Description Text
         descriptionText = new JLabel("Description");
-        descriptionText.setBounds(((rightPanel.getWidth() + 10)/2)+5, 240, 350, 30);
+        descriptionText.setBounds(70, 240, 350, 30);
         descriptionText.setFont(new Font("Times New Roman", Font.BOLD, 16));
 
-        // Input Section
-        // Username Display
-        userDisplay = new JTextField();
-        userDisplay.setBounds((rightPanel.getWidth() + 10)/2, 130, 600, 30);
+        // Combo Box Section
+        // User Combo Box
+        String[] username = {"Darien", "SS"};
+        usernameBox = new JComboBox<>(username);
+        usernameBox.setBounds(65, 130, 600, 30);
+        usernameBox.addActionListener(this);
 
+        // Input Section
         // Date Display
         dateDisplay = new JTextField();
-        dateDisplay.setBounds((rightPanel.getWidth() + 10)/2, 200, 600, 30);
+        dateDisplay.setBounds(65, 200, 600, 30);
 
         // Description Display
         descriptionDisplay = new JTextArea(20, 50);
@@ -149,30 +108,23 @@ public class PageTechnician_Feedback implements ActionListener{
 
         // Scroll Pane for Description Display
         JScrollPane scrollPane2 = new JScrollPane(descriptionDisplay);
-        scrollPane2.setBounds(25, 270, 600, 250);
+        scrollPane2.setBounds(0, 0, descriptionPanel.getWidth(), descriptionPanel.getHeight());
 
 
 
 
 
         // Add Section
-        // Left Top Panel
-        container.add(customerText);
-        container.add(scrollPane);
-        container.add(leftTopPanel);
-
-        // Left Below Panel
-        leftBelowPanel.add(table);
 
         // Right Panel
-        rightPanel.add(scrollPane2);
+        descriptionPanel.add(scrollPane2);
         container.add(feedbackText);
         container.add(userText);
-        container.add(userDisplay);
+        container.add(usernameBox);
         container.add(dateText);
         container.add(dateDisplay);
         container.add(descriptionText);
-        container.add(rightPanel);
+        container.add(descriptionPanel);
 
         // Container
         container.add(icon);
